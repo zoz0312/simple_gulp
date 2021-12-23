2a70464e298161fce96d1640094b40e68e843c13
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-12-23 20:32:52 +0900

    gulp babel setting

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,2 +1,18 @@
 import gulp from 'gulp';
+import gulpPug from 'gulp-pug';
 
+const routes = {
+  pug: {
+    src: 'src/*.pug',
+    dest: 'build'
+  }
+}
+
+export const pug = () => {
+  return gulp
+    .src(routes.pug.src)
+    .pipe(gulpPug())
+    .pipe(gulp.dest(routes.pug.dest));
+}
+
+export const dev = gulp.series([pug]);
