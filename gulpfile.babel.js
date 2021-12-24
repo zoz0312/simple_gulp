17f6ee73f580955544aa257926ca24e95586758b
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-12-25 03:51:54 +0900

    gulp library add

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,40 @@
 import gulp from 'gulp';
 import gulpPug from 'gulp-pug';
+import del from 'delete';
+import ws from 'gulp-webserver';
 
 const routes = {
   pug: {
+    watch: 'src/**/*.pug',
     src: 'src/*.pug',
     dest: 'build'
   }
 }
 
-export const pug = () => {
+const pug = () => {
   return gulp
     .src(routes.pug.src)
     .pipe(gulpPug())
     .pipe(gulp.dest(routes.pug.dest));
 }
 
-export const dev = gulp.series([pug]);
+const clean = () => del(['build/']);
+
+const webserver = () => gulp.src('build').pipe(ws({
+  host: 'localhost',
+  port: '8080',
+  livereload: true,
+  // open: true,
+}));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+}
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+const postDev = gulp.parallel([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
