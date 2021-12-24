4a15868f4897555e0d8db24be992b41f1429d302
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2021-12-25 04:53:39 +0900

    [ADD] gulp javascript minify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,10 +4,13 @@ import gulpImage from 'gulp-image';
 import gulpSass from 'gulp-sass';
 import gulpAutoPrefixer from 'gulp-autoprefixer';
 import gulpMinifyCss from 'gulp-csso';
+import gulpBro from 'gulp-bro';
+
 import del from 'delete';
 import ws from 'gulp-webserver';
 import defaultSass from 'sass';
 import nodeSass from 'node-sass';
+import babelify from 'babelify';
 
 const sass = gulpSass(defaultSass);
 
@@ -27,6 +30,11 @@ const routes = {
     watch: 'src/scss/**/*.scss',
     src: 'src/scss/styles.scss',
     dest: 'build/css',
+  },
+  js: {
+    watch: 'src/js/**/*.js',
+    src: 'src/js/**/*.js',
+    dest: 'build/js',
   }
 }
 
@@ -46,7 +54,6 @@ const webserver = () => gulp.src('build').pipe(ws({
   // open: true,
 }));
 
-
 const img =() => gulp
   .src(routes.img.src)
   .pipe(gulpImage())
@@ -59,14 +66,25 @@ const styles = () => gulp
   .pipe(gulpMinifyCss())
   .pipe(gulp.dest(routes.scss.dest))
 
+const js = () => gulp
+  .src(routes.js.src)
+  .pipe(gulpBro({
+    transform:  [
+      babelify.configure({ presets: ['@babel/preset-env'] }),
+      ['uglifyify', { global: true }],
+    ],
+  }))
+  .pipe(gulp.dest(routes.js.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js)
 }
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug, styles]);
+const assets = gulp.series([pug, styles, js]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
